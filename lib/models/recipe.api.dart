33e250977bdb09d368e9ace8f0337648dd7472dd
Jakub Kobialka <kobialka.kontakt@gmail.com>
2024-05-16 19:43:58 +0200

    add fetching recipies list

---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:foodsi_app/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+
+  static Future<List<Recipe>> getRecipies() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list', {
+      'limit': '10',
+      'start': '0'
+    });
+
+    final response = await http.get(uri, headers: {
+      'X-RapidAPI-Key': '152ac0106dmsh41ab860fa196ac3p1bf2c9jsne6c9b9e93526',
+      'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+    });
+    
+    Map data = jsonDecode(response.body);
+    List _tmp = [];
+
+    for(var i in data['feed'])
+    {
+      _tmp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_tmp);
+  }
+}
--- lib/views/recipies_list.dart
+++ lib/views/recipies_list.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:foodsi_app/models/recipe.api.dart';
+import 'package:foodsi_app/models/recipe.dart';
+import 'package:foodsi_app/widgets/recipe_card.dart';
 
 class RecipiesList extends StatefulWidget {
   const RecipiesList({super.key});
@@ -8,6 +11,16 @@ class RecipiesList extends StatefulWidget {
 }
 
 class _RecipiesListState extends State<RecipiesList> {
+
+  late Future<List<Recipe>> _futureRecipies;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _futureRecipies = RecipeApi.getRecipies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +32,29 @@ class _RecipiesListState extends State<RecipiesList> {
           SizedBox(width: 10),
           Text('Foodsi'),
         ],),
+      ),
+      body: FutureBuilder<List<Recipe>>(
+        future: _futureRecipies,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            List<Recipe> recipies = snapshot.data ?? [];
+            return ListView.builder(
+              itemCount: recipies.length,
+              itemBuilder: (context, index) {
+                return RecipeCard(
+                  title: recipies[index].name,
+                  cookTime: recipies[index].totalTime,
+                  rating: recipies[index].rating.toString(),
+                  thumbnailURL: recipies[index].images,
+                );
+              },
+            );
+          }
+        },
       )
     );
   }
